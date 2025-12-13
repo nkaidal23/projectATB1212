1199a6098da1803e3c9710ce60cd37e32ab974c9
HemangiKaidal <147647542+HemangiKaidal@users.noreply.github.com>
2025-12-13 13:03:15 +0530

    my first commit

--- src/JAVABASICS_001/test.java
+++ src/JAVABASICS_001/test.java
@@ -1,7 +1,7 @@
 package JAVABASICS_001;
 
-public class basicsjava {
+public class test {
     public static void main(String[] args) {
-        System.out.println("neha");
+        System.out.println(hello);
     }
 }
