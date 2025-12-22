25f5d954eeaa4356b46e978a4111dfc23a045dd1
HemangiKaidal <147647542+HemangiKaidal@users.noreply.github.com>
2025-12-22 20:11:28 +0530

    17th may

---
+++ src/JavaBasics_04_Operator/java_22_relational_operator.java
@@ -0,0 +1,21 @@
+package JavaBasics_04_Operator;
+
+public class java_22_relational_operator {
+    public static void main(String[] args) {
+      //  <
+        //<=
+        //>
+        //>=
+        //!=
+// all of them always result in boolean
+        int a = 10 ;
+        int b = 20;
+        boolean c = a > b;
+        System.out.println(c);
+
+        int age_mamta = 70;
+        int age_pramod = 10 ;
+        boolean d = (age_mamta>=age_pramod);
+        System.out.println(d);
+    }
+}
